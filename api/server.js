f1f1de6ac5850f4128b72b8beac8412434a9d0b7
Rahul Choudhary <rahulchoudhary0775@gmail.com>
2024-08-12 11:30:05 +0530

    added path

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
+const path = require('path');
 const { Client } = require('pg');
 require('dotenv').config()
 
