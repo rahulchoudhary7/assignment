12fbf215ab87b12ac0065d4220b48be789ba48fa
Rahul Choudhary <rahulchoudhary0775@gmail.com>
2024-08-12 11:38:19 +0530

    updated server.js for deployment

--- api/server.js
+++ api/server.js
@@ -11,11 +11,11 @@ app.use(cors({
 }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.join(__dirname, '/client/dist')))
 
-app.get('*', (req, res) =>{
-  res.sendFile(path.join(__dirname, '../build', 'index.html'));
-});
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
 
 const config = {
     user: process.env.DB_USER,
